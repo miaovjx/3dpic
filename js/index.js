1cdfdcb28b1adad9c4a8f148694a8a1b76e75052
JSON <610330335@qq.com>
2017-08-04 21:29:44 +0800

    add swiper

--- js/index.js
+++ js/index.js
@@ -19,9 +19,8 @@ $(function() {
             $('.page1').removeClass('none');
         }, 500);
     });
-
     /*3d 滚动 +弹窗*/
-  var jq=jQuery.noConflict();  
+    var jq = jQuery.noConflict();
     jq(document).ready(function() {
         jq('.roundabout_box ul').roundabout({
             duration: 1000,
@@ -37,12 +36,61 @@ $(function() {
             enableDrag: true
         });
     });
-
-    jq('.roundabout-holder li').on('tap', function() {
-        
+    jq('.roundabout-holder li').on('tap', function() {});
+    var swipercd = null;
+    var swiperwh = null;
+    var swiperhf = null;
+    $('.roundabout-in-focus').live('tap', function() {
+        var city = $(this).attr('data-city');
+        // alert(city)
+        if (city == 'cd') {
+            $('.tk-detail').addClass('none');
+            $('.tk-detail-cd').removeClass('none');
+            if (swipercd) {
+                swipercd.destroy()
+            } else {
+                setTimeout(function() {
+                    swipercd = new Swiper('.swiper-container-cd', {
+                        pagination: '.swiper-pagination',
+                        slidesPerView: 1,
+                        spaceBetween: 0,
+                        loop: true
+                    })
+                }, 100)
+            }
+        } else if (city == 'wh') {
+            $('.tk-detail').addClass('none');
+            $('.tk-detail-wh').removeClass('none');
+            if (swiperwh) {
+                swiperwh.destroy()
+            } else {
+                setTimeout(function() {
+                    swiperwh = new Swiper('.swiper-container-wh', {
+                        pagination: '.swiper-pagination',
+                        slidesPerView: 1,
+                        spaceBetween: 0,
+                        loop: true
+                    })
+                }, 100)
+            }
+        } else if (city == 'hf') {
+            $('.tk-detail').addClass('none');
+            $('.tk-detail-hf').removeClass('none');
+            if (swiperhf) {
+                swiperhf.destroy()
+            } else {
+                setTimeout(function() {
+                    swiperhf = new Swiper('.swiper-container-hf', {
+                        pagination: '.swiper-pagination',
+                        slidesPerView: 1,
+                        spaceBetween: 0,
+                        loop: true
+                    })
+                }, 100)
+            }
+        }
     });
-
-    $('.btn_close,.btn_cancel').on('tap', function() {
+    $('.btn_close').on('tap', function() {
         $(this).closest('.tk').addClass('none');
     });
 });
