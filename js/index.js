5ba766d04409244e3a53dbcec0d104706d63dfb8
JSON <610330335@qq.com>
2017-08-07 12:04:17 +0800

    error

--- js/index.js
+++ js/index.js
@@ -1,26 +1,31 @@
-/**
- * Created by json(610330335@qq.com) .
- */
-$(function() {
-    //加载图
-    var imgarr = ['images/bg1.jpg'];
-    //app初始化
-    var h5 = new PageSlider({
-        pages: $('.page-wrap .page'),
-        dev: 0, //
-        // musicUrl: 'music/bg.mp3',
-        baseUrl: 'http://xk.guoxinad.com.cn/ad_invitation/'
-    });
-    ////默认分享
-    h5.wxShare('宝贝性格测试，你是否真懂你的娃？', '解自家娃！', '宝贝性格测试，你是否真懂你的娃？', h5.baseUrl + 'index.html', h5.baseUrl + 'images/jsshare.jpg');
-    h5._loadimg(imgarr, function() {
-        setTimeout(function() {
-            $('.loading').addClass('none');
-            $('.page1').removeClass('none');
-        }, 500);
-    });
 
 
+/*动态获取地址*/
+var imgarr = [''];
+var h5 = new PageSlider({
+	pages: $('.page-wrap .page'),
+	dev: 0,
+	// musicUrl: 'music/bg.mp3',
+	baseUrl: 'https://yidian.weiyihui.com.cn/bnq_fengge/',
+	onchange: function() { //每一屏切换完成时的回调
+		// console.log(this.index)
+		// alert(this.index)
+		if(this.index == 0) {
+
+		} else if(this.index == 4) {
+			   
+		}
+	}
+});
+
+h5._loadimg(imgarr, function() {
+	setTimeout(function() {
+		$('#loading').addClass("none");
+		$('.page1').removeClass("none");
+		//h5._init();
+	}, 500);
+});
+//---------------------------------------------------------
 
     /*3d 滚动 +弹窗*/
     var cindex = 2;
@@ -70,10 +75,11 @@ $(function() {
                         pagination: '.swiper-pagination',
                         slidesPerView: 1,
                         spaceBetween: 0,
-                        effect: 'fade',                    
+                        effect: 'fade',
+                       
                         loop: false
                     })
-                    swiper.slideTo(index,500, false)
+                    swiper.slideTo(index,500, false);
                 }, 100)
             }
         });
@@ -83,5 +89,6 @@ $(function() {
     });
 
 
-    /*3d 滚动 +弹窗*/
-});
+
+//----------------------------------------------------------------------------------------
+
